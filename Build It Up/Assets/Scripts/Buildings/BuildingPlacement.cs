0d47c3e53b7fa5b34ac518a9e73ecbdf761ae26c
Lolboy30 <simonpumpernick@outlook.com>
2016-02-01 17:35:21 +0100

    First Commit

---
+++ Build It Up/Assets/Scripts/Buildings/BuildingPlacement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+    private PlaceableBuildings placeableBuilding;
+    private Transform currentBuilding;
+    private bool hasPlaced;
+
+    public LayerMask buildingsMask;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 m = Input.mousePosition;
+        m = new Vector3(m.x, m.y, transform.position.y);
+        Vector3 p = GetComponent<Camera>().ScreenToWorldPoint(m);
+
+        if (currentBuilding != null && !hasPlaced)
+        {
+            currentBuilding.position = new Vector3(p.x, 0, p.z);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsLegalPosition())
+                {
+                hasPlaced = true;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                Ray ray = new Ray(new Vector3(p.x, 500, p.z), Vector3.down);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsMask))
+                {
+                    Debug.Log(hit.collider.name);
+                }
+            }
+        }
+	}
+
+    bool IsLegalPosition()
+    {
+        if (placeableBuilding.colliders.Count > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void SetItem(GameObject b)
+    {
+        hasPlaced = false;
+        currentBuilding = ((GameObject)Instantiate(b)).transform;
+        placeableBuilding = currentBuilding.GetComponent<PlaceableBuildings>();
+    }
+}
---
+++ Build It Up/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float cameraHight;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(transform.position.x, cameraHight, transform.position.z);
+        
+        float horizontal = Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * vertical, Space.World);
+        transform.Translate(Vector3.right * horizontal, Space.World);
+    }
+}
